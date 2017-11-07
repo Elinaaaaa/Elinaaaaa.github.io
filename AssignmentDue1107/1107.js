c954ff1440466dadaa6fca2306193e915b3f5919
刘佳怡 <liujiayi@liujiayideMBP.wireless.newschool.edu>
2017-11-07 09:53:52 -0500

    1107

--- AssignmentDue1107/1107.js
+++ AssignmentDue1107/1107.js
@@ -11,3 +11,57 @@ function clickme2(){
 	document.getElementById("length").innerHTML = triangle.push("▲");
 	document.getElementById("array1").innerHTML = triangle.join(" ")
 }
+
+function clickme3(){
+	document.getElementById("length").innerHTML = triangle.push("◆");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+function clickme4(){
+	document.getElementById("length").innerHTML = triangle.push("◇");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+function clickme5(){
+	document.getElementById("length").innerHTML = triangle.push("☁");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+function clickme6(){
+	document.getElementById("length").innerHTML = triangle.push("■");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+function clickme7(){
+	document.getElementById("length").innerHTML = triangle.push("□");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+function clickme8(){
+	document.getElementById("length").innerHTML = triangle.push("☼");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+function clickme9(){
+	document.getElementById("length").innerHTML = triangle.push("☀");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+function clickme10(){
+	document.getElementById("length").innerHTML = triangle.push("◐");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+function clickme11(){
+	document.getElementById("length").innerHTML = triangle.push("◑");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+function clickme12(){
+	document.getElementById("length").innerHTML = triangle.push("♡");
+	document.getElementById("array1").innerHTML = triangle.join(" ")
+}
+
+
+
+
